2ad02644e70d31ba5521dd044efb1693983cb213
mikewanjero <mikewanj@gmail.com>
2024-05-07 18:23:20 +0300

    Added GoalInput: TextInput Box, Button and Function Calls for Button Press

---
+++ components/goalInput.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { TextInput, View, Button, StyleSheet } from "react-native";
+
+export default function GoalInput(props) {
+  const [enteredGoalText, setEnteredGoalText] = useState("");
+
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  }
+
+  function AddGoalHandler() {
+    props.onAddGoal(enteredGoalText);
+    setEnteredGoalText("");
+  }
+
+  return (
+    <View style={styles.inputContainer}>
+      <TextInput
+        style={styles.textInput}
+        placeholder="Your course goal!"
+        onChangeText={goalInputHandler}
+        value={enteredGoalText}
+      />
+      <Button title="Add Goal" onPress={AddGoalHandler} />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#cccccc",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "70%",
+    marginRight: 10,
+    padding: 10,
+  },
+});
